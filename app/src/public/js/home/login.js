d2750881e5f9029eb5e798c3aeb7a7728ff6eb9a
BuildTools <unconfigured@null.spigotmc.org>
2022-09-10 17:31:25 +0900

    testy

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,12 @@
-console.log("test");
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+}
